b01cab103485ac0140afed5d52ef273bb862ed52
woorin01 <woorin0101@gmail.com>
2019-09-02 17:54:59 +0900

    no message

--- New Unity Project/Assets/NewBehaviourScript.cs
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -19,30 +19,29 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray.origin, ray.direction, out hit))
+        if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit))
         {
             Debug.Log(hit.point);
             if (hit.transform.gameObject.CompareTag("Player"))
             {
                 int x = 0, y = 0, z = 0;
+                Vector3 htp = hit.transform.position;
 
-                if (hit.transform.position.x + 0.4f <= hit.point.x) { x = 1; y = 0; z = 0; }
-                else if (hit.transform.position.x - 0.4f >= hit.point.x) { x = -1; y = 0; z = 0; }
+                if (htp.x + 0.4f <= hit.point.x) { x = 1; y = 0; z = 0; }
+                else if (htp.x - 0.4f >= hit.point.x) { x = -1; y = 0; z = 0; }
 
-                if (hit.transform.position.y + 0.4f <= hit.point.y) { x = 0; y = 1; z = 0; }
-                else if (hit.transform.position.y - 0.4f >= hit.point.y) { x = 0; y = -1; z = 0; }
+                if (htp.y + 0.4f <= hit.point.y) { x = 0; y = 1; z = 0; }
+                else if (htp.y - 0.4f >= hit.point.y) { x = 0; y = -1; z = 0; }
 
-                if (hit.transform.position.z + 0.4f <= hit.point.z) { x = 0; y = 0; z = 1; }
-                else if (hit.transform.position.z - 0.4f >= hit.point.z) { x = 0; y = 0; z = -1; }
+                if (htp.z + 0.4f <= hit.point.z) { x = 0; y = 0; z = 1; }
+                else if (htp.z - 0.4f >= hit.point.z) { x = 0; y = 0; z = -1; }
 
-                mCube.transform.position = new Vector3(hit.transform.position.x + x, hit.transform.position.y + y, hit.transform.position.z + z);
+                mCube.transform.position = new Vector3(htp.x + x, htp.y + y, htp.z + z);
 
                 if (Input.GetMouseButtonUp(0))
-                    Instantiate<GameObject>(Resources.Load<GameObject>("Cube")).transform.position = new Vector3(hit.transform.position.x + x, hit.transform.position.y + y, hit.transform.position.z + z);
+                    Instantiate<GameObject>(Resources.Load<GameObject>("Cube")).transform.position = new Vector3(htp.x + x, htp.y + y, htp.z + z);
 
                 mCube.SetActive(true);
             }
