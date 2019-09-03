d7a3e792fe314dd8b89f1bc8ff1358256e307518
woorin01 <woorin0101@gmail.com>
2019-09-03 12:14:00 +0900

    1.중복설치 불가 2.배경변경 3.블럭을 관통해 고스트 블럭이 나오는 버그 픽

--- New Unity Project/Assets/BlockManager.cs
+++ New Unity Project/Assets/BlockManager.cs
@@ -6,7 +6,6 @@ public class BlockManager : MonoBehaviour
 {
     public GameObject mCube;
     public GameObject[,,] mBlocks = new GameObject[200, 200, 200];
-
     private void Awake()
     {
         mCube.SetActive(false);
@@ -29,7 +28,7 @@ public class BlockManager : MonoBehaviour
 
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));//Ray를 쏠 지점을 화면의 정가운데로 한다
 
         if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit))
         {
@@ -38,25 +37,24 @@ public class BlockManager : MonoBehaviour
                 int x = 0, y = 0, z = 0;
                 Vector3 htp = hit.transform.position;
 
-                if (htp.x + 0.4f <= hit.point.x && IsBlockNone((int)htp.x + 1, (int)htp.y, (int)htp.z)) { x = 1; y = 0; z = 0; }
-                else if (htp.x - 0.4f >= hit.point.x && IsBlockNone((int)htp.x - 1, (int)htp.y, (int)htp.z)) { x = -1; y = 0; z = 0; }
+                if (htp.x + 0.5f <= hit.point.x && IsBlockNone((int)htp.x + 1, (int)htp.y, (int)htp.z)) { x = 1; y = 0; z = 0; }
+                else if (htp.x - 0.5f >= hit.point.x && IsBlockNone((int)htp.x - 1, (int)htp.y, (int)htp.z)) { x = -1; y = 0; z = 0; }
 
-                if (htp.y + 0.4f <= hit.point.y && IsBlockNone((int)htp.x, (int)htp.y + 1, (int)htp.z)) { x = 0; y = 1; z = 0; }
-                else if (htp.y - 0.4f >= hit.point.y && IsBlockNone((int)htp.x, (int)htp.y - 1, (int)htp.z)) { x = 0; y = -1; z = 0; }
+                if (htp.y + 0.5f <= hit.point.y && IsBlockNone((int)htp.x, (int)htp.y + 1, (int)htp.z)) { x = 0; y = 1; z = 0; }
+                else if (htp.y - 0.5f >= hit.point.y && IsBlockNone((int)htp.x, (int)htp.y - 1, (int)htp.z)) { x = 0; y = -1; z = 0; }
 
                 if (htp.z + 0.5f <= hit.point.z && IsBlockNone((int)htp.x, (int)htp.y, (int)htp.z + 1)) { x = 0; y = 0; z = 1; }
                 else if (htp.z - 0.5f >= hit.point.z && IsBlockNone((int)htp.x, (int)htp.y, (int)htp.z - 1)) { x = 0; y = 0; z = -1; }
 
                 mCube.transform.position = new Vector3(htp.x + x, htp.y + y, htp.z + z);
 
-                if (Input.GetMouseButtonUp(1))
+                if (Input.GetMouseButtonUp(1) && IsBlockNone((int)htp.x + x, (int)htp.y + y, (int)htp.z + z))
                 {
                     GameObject temp = Instantiate<GameObject>(Resources.Load<GameObject>("Cube"));
                     temp.transform.position = new Vector3(htp.x + x, htp.y + y, htp.z + z);
                     mBlocks[(int)htp.x + x, (int)htp.y + y, (int)htp.z + z] = temp;
                 }
 
-                mCube.SetActive(true);
             }
         }
         else
@@ -67,8 +65,12 @@ public class BlockManager : MonoBehaviour
     private bool IsBlockNone(int x, int y, int z)
     {
         if (mBlocks[x, y, z] != null)
+        {
+            mCube.SetActive(false);
             return false;
+        }
 
+        mCube.SetActive(true);
         return true;
     }
 }
