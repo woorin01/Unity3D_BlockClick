092b556b7b6bb279b4ecff0c2546cba1be6c3d04
woorin01 <woorin0101@gmail.com>
2019-09-24 16:06:22 +0900

    no message

--- New Unity Project/Assets/Resources/Scripts/BlockManager.cs
+++ New Unity Project/Assets/Resources/Scripts/BlockManager.cs
@@ -20,6 +20,8 @@ public class BlockManager : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;//마우스 커서 고정
         Cursor.visible = false;
+        
+        //start1, coroutine1, start2, update1, update1, coroutine2
     }
 
     void Start()
@@ -37,85 +39,118 @@ public class BlockManager : MonoBehaviour
 
     void Update()
     {
-        int layerMask = 1 << LayerMask.NameToLayer("TranslucentBlock");// TranslucentBlock레이어만 무시하고 충돌
-        layerMask = ~layerMask;
+        if (Input.GetMouseButtonDown(1))
+            ClickMakeBlock();
 
-        //Ray ray = new Ray(transform.position, transform.forward);
-        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));//Ray를 쏠 지점을 화면의 정가운데로 한다
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButton(0))
+            DestroyBlock();
 
-        //ray.direction += new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f)); //ray.direction의 값은 normalized 되어 있기 때문에 쏘는 방향을 다르게 해주려면 저렇게 해줘야됨
+        if (Input.GetMouseButtonUp(0))
+            StopDestroyBlock(hitBlock);
 
-        if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, 7f, layerMask))
+    }
+
+    private void ClickMakeBlock()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, 5.5f))
         {
             if (hit.transform.gameObject.CompareTag("Player"))
             {
                 Vector3 installPos = hit.transform.position;
                 installPos += hit.normal;
+                
+                foreach (Transform child in map)
+                    if (child.position.Equals(installPos))
+                        return;
 
-                if (Input.GetMouseButtonDown(1))
-                    ClickMakeBlock(installPos);
+                if (Vector3.Distance(transform.position, installPos) <= 1.05f)
+                    return;
 
-                if (Input.GetMouseButton(0))
-                    DestroyBlock(hit.transform.GetComponent<block>());
-                if (Input.GetMouseButtonUp(0))
-                    StopDestroyBlock(hit.transform.GetComponent<block>());
+                Debug.Log(Vector3.Distance(transform.position, installPos));
+                GameObject temp = Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/" + changeImage.imageNum.ToString()));
+                temp.transform.position = installPos;
+                temp.transform.parent = map;
+                temp.GetComponent<MeshRenderer>().material.color = changeImage.color;
             }
         }
-
-        Debug.DrawRay(ray.origin, ray.direction * 5f, Color.blue, 0.3f);
     }
 
-    private void ClickMakeBlock(Vector3 installPos)
+    private void DestroyBlock()
     {
-        if (Vector3.Distance(transform.position, installPos) <= 1.05f)
-            return;
-
-        Debug.Log(Vector3.Distance(transform.position, installPos));
-        GameObject temp = Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/" + changeImage.imageNum.ToString()));
-        temp.transform.position = installPos;
-        temp.transform.parent = map;
-        temp.GetComponent<MeshRenderer>().material.color = changeImage.color;
-    }
-
-    private void DestroyBlock(block block)
-    {
-        if (hitBlock == null)
-            hitBlock = block;
-        if (hitBlock != block)
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, 7f))
         {
-            StopDestroyBlock(hitBlock);
-            hitBlock = block;
-        }
+            if (hit.transform.gameObject.CompareTag("Player"))
+            {
+                block block = hit.transform.GetComponent<block>();
+                if (hitBlock == null)
+                    hitBlock = block;
+                if (hitBlock != block)
+                {
+                    StopDestroyBlock(hitBlock);
+                    hitBlock = block;
+                }
 
-        hitBlock.healthPoint--;
-        Debug.Log(hitBlock.healthPoint);
-        if (hitBlock.healthPoint <= 0f)
-            Destroy(hitBlock.gameObject);
+                hitBlock.healthPoint--;
+                Debug.Log(hitBlock.healthPoint);
+                if (hitBlock.healthPoint <= 0f)
+                    Destroy(hitBlock.gameObject);
+            }
+        }
     }
 
     private void StopDestroyBlock(block block)
     {
+        if (block == null)
+            return;
+
         block.healthPoint = block.maxHealthPoint;
     }
 
-    private void ClickDestroyBlock(block block, Vector3 htp)
-    {
-        if (hitBlock == block)
-            Debug.Log("same block");
-        else
-        {
-            if (hitBlock == null)
-                hitBlock = block;
+    //private void ClickDestroyBlock(block block, Vector3 htp)
+    //{
+    //    if (hitBlock == block)
+    //        Debug.Log("same block");
+    //    else
+    //    {
+    //        if (hitBlock == null)
+    //            hitBlock = block;
 
-            hitBlock.healthPoint = hitBlock.maxHealthPoint;
-        }
+    //        hitBlock.healthPoint = hitBlock.maxHealthPoint;
+    //    }
 
-        hitBlock = block;
-        hitBlock.healthPoint--;
+    //    hitBlock = block;
+    //    hitBlock.healthPoint--;
 
-        if (hitBlock.healthPoint <= 0f)
-            Destroy(block);
+    //    if (hitBlock.healthPoint <= 0f)
+    //        Destroy(block);
 
-    }
+    //}
 }
+
+//int layerMask = 1 << LayerMask.NameToLayer("TranslucentBlock");// TranslucentBlock레이어만 무시하고 충돌
+//layerMask = ~layerMask;
+
+//Ray ray = new Ray(transform.position, transform.forward);
+//Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));//Ray를 쏠 지점을 화면의 정가운데로 한다
+//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+//ray.direction += new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f)); //ray.direction의 값은 normalized 되어 있기 때문에 쏘는 방향을 다르게 해주려면 저렇게 해줘야됨
+
+//if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, 7f, layerMask))
+//{
+//    if (hit.transform.gameObject.CompareTag("Player"))
+//    {
+//        Vector3 installPos = hit.transform.position;
+//        installPos += hit.normal;
+
+//        if (Input.GetMouseButtonDown(1))
+//            ClickMakeBlock(installPos);
+
+//        if (Input.GetMouseButton(0))
+//            DestroyBlock(hit.transform.GetComponent<block>());
+//        if (Input.GetMouseButtonUp(0))
+//            StopDestroyBlock(hit.transform.GetComponent<block>());
+//    }
+//}
